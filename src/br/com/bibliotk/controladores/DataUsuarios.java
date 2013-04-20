f4fc522c4ec07c9e6c1b56bf16ff1cb678b9c045
Rafael Garcia <rafbgarcia@gmail.com>
2013-04-20 01:11:54 -0300

    Pacotes seguindo padrão de nomeação

---
+++ src/br/com/bibliotk/controladores/DataUsuarios.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.bibliotk.controladores;
+
+import br.com.bibliotk.models.Usuario;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Ivanilson
+ */
+public class DataUsuarios {
+
+}
---
+++ src/br/com/bibliotk/models/Usuario.java
@@ -0,0 +1,161 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.bibliotk.models;
+
+import br.com.bibliotk.enums.Sexo;
+import br.com.bibliotk.enums.UF;
+import java.util.Date;
+
+/**
+ *
+ * @author Ivanilson
+ */
+public class Usuario {
+    private int id;
+    private String nome;
+    private String cpf;
+    private Date dataNascimento;
+    private Sexo sexo;
+    private String email;
+    private String telefone;
+    private String celular;
+    private String rua;
+    private int num;
+    private String bairro;
+    private String complemento;
+    private UF uf;
+    private String municipio;
+    private Date dataCadastro;
+    private boolean aptoEmprestimo;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Sexo getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(Sexo sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public UF getUf() {
+        return uf;
+    }
+
+    public void setUf(UF uf) {
+        this.uf = uf;
+    }
+
+    public String getMunicipio() {
+        return municipio;
+    }
+
+    public void setMunicipio(String municipio) {
+        this.municipio = municipio;
+    }
+
+    public Date getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(Date dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public boolean isAptoEmprestimo() {
+        return aptoEmprestimo;
+    }
+
+    public void setAptoEmprestimo(boolean aptoEmprestimo) {
+        this.aptoEmprestimo = aptoEmprestimo;
+    }
+    
+}
